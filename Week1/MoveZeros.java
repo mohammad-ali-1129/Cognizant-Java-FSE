c6a8a3e159b971e4b6cb93bc8a210026249c913a
Mohammad Ali <aliuser1129@gmail.com>
2026-06-24 22:35:18 +0530

    Learnt about two pointr technique and solved move zeros problem using the tecchnique

---
+++ Week1/MoveZeros.java
@@ -0,0 +1,22 @@
+public class MoveZeros {
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 0, 3, 12};
+
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
