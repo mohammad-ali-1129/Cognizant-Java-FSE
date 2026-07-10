2eb95894af8bf9968b6dfa603728a107b066f8d7
Mohammad Ali <aliuser1129@gmail.com>
2026-07-10 11:20:43 +0530

    Complete Hands-on 2: Load SimpleDateFormat using Spring XML

---
+++ Week3/Handson2_DateFormat/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,54 @@
+package com.cognizant.springlearn;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("START");
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayDate();
+
+        LOGGER.info("END");
+    }
+
+    public static void displayDate() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+        try {
+
+            Date date = format.parse("31/12/2018");
+
+            LOGGER.debug("Date : {}", date);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        LOGGER.info("END");
+    }
+}
