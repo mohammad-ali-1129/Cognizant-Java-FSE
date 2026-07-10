f2475e0b001f6a5dc0bbf945085d546cafb3c2d0
Mohammad Ali <aliuser1129@gmail.com>
2026-07-10 14:38:50 +0530

    Complete Hands-on 6: Load list of countries from Spring XML

---
+++ Week3/Handson6_CountryList/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,91 @@
+package com.cognizant.springlearn;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("START");
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayDate();
+
+        displayCountries();
+
+        LOGGER.info("END");
+    }
+
+    public static void displayDate() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+        try {
+
+            Date date = format.parse("31/12/2018");
+
+            LOGGER.debug("Date : {}", date);
+
+        } catch (Exception e) {
+
+            LOGGER.error("Error while parsing date", e);
+
+        }
+
+        LOGGER.info("END");
+    }
+
+    public static void displayCountry() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        Country anotherCountry = context.getBean("country", Country.class);
+
+        LOGGER.debug("Country 1 : {}", country);
+
+        LOGGER.debug("Country 2 : {}", anotherCountry);
+
+        LOGGER.info("END");
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void displayCountries() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        ArrayList<Country> countryList =
+                (ArrayList<Country>) context.getBean("countryList");
+
+        LOGGER.debug("Countries : {}", countryList);
+
+        LOGGER.info("END");
+    }
+}
