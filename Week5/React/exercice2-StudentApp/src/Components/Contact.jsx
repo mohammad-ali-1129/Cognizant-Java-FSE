d5bbf72a56fee06e40ae5937c1b11d335e80e7d3
Mohammad Ali <aliuser1129@gmail.com>
2026-07-22 14:19:48 +0530

    add StudentApp React components using Vite

---
+++ Week5/React/exercice2-StudentApp/src/Components/Contact.jsx
@@ -0,0 +1,7 @@
+function Contact() {
+  return (
+    <h3>Welcome to the Contact page of the Student Management Portal</h3>
+  );
+}
+
+export default Contact;
---
+++ Week5/React/exercice2-StudentApp/src/Components/Home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <h3>Welcome to the Home page of Student Management Portal</h3>
+  );
+}
+
+export default Home;
